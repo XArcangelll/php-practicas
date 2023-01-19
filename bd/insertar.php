d7c343202d7224fae3be0744e72788f5c2979b00
XArcangelll <Xx-Arcangel@hotmail.com>
2023-01-19 17:20:04 -0500

    todo list completado

--- bd/insertar.php
+++ bd/insertar.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>insertar</title>
+
+    <style>
+        .deshabilitado{
+            color: #aaa;
+        }
+    </style>
 </head>
 <body>
 
@@ -12,6 +18,20 @@
     <input type="text" name="texto" id="texto">
     <input type="submit" value="Añadir Pendiente">
 </form>
+<div id="mostrar-todo-container">
+        <form id="formMostrarTodo" action="insertar.php" method="POST">
+                <input type="checkbox" name="mostrar-todo" onchange="mostrarTodo(this)"
+                <?php
+                if(isset($_POST["mostrar-todo"])){
+                    if($_POST["mostrar-todo"]== "on"){
+                        echo "checked";
+                    }
+                }
+                ?>
+                
+                > Mostrar Todo
+        </form>
+</div>
 
 <div id="todoList">
     <?php
@@ -28,14 +48,17 @@
 
       if(isset($_POST["texto"])){
         $texto = $_POST["texto"];
-        $sql = "INSERT INTO todoTable(texto,completado) VALUES('$texto',false)";
-        if($conexion->query($sql)===true){
-
-           // echo '<div><form action=""><input type="checkbox">'. $texto .'</form></div>';
+        if($texto != "") {
+            $sql = "INSERT INTO todoTable(texto,completado) VALUES('$texto',false)";
+            if($conexion->query($sql)===true){
+    
+               // echo '<div><form action=""><input type="checkbox">'. $texto .'</form></div>';
+            }
+            else{
+                die("Error al insertar datos: " . $conexion->error);
+            }  
         }
-        else{
-            die("Error al insertar datos: " . $conexion->error);
-        }  
+      
       }else if(isset($_POST["completar"])) {
             $id = $_POST["completar"];
             $sql = "UPDATE todoTable SET completado =  1 WHERE id = $id";
@@ -61,7 +84,16 @@
          }  
       }
 
-      $sql = "SELECT * FROM todotable where completado = 0";
+      $sql = "";
+       if(isset($_POST["mostrar-todo"])){
+        $ordenar = $_POST["mostrar-todo"];
+        if($ordenar== "on"){
+            $sql = "SELECT * FROM todoTable ORDER BY completado desc";
+      }
+    }else{
+        $sql = "SELECT * FROM todoTable WHERE completado = 0 ";
+      }
+
       $resultado = $conexion->query($sql);
 
       if($resultado->num_rows > 0){
@@ -69,7 +101,13 @@
                 ?>
                    <div>
                         <form method="POST" id="form<?php echo $row['id']?>" action="">
-                             <input name="completar" value="<?php echo $row['id']?>" id="<?php echo $row['id']?>" type="checkbox" onchange="completarPendiente(this)"><?php echo $row['texto']?>
+                             <input name="completar" value="<?php echo $row['id']?>" id="
+                             <?php echo $row['id']?>" 
+                             type="checkbox" onchange="completarPendiente(this)" 
+                             <?php if($row["completado"] == 1)echo "checked disabled"; ?> >
+                             <div class="texto
+                              <?php if($row["completado"] == 1)echo "deshabilitado"; ?>
+                              " ><?php echo $row['texto'];?></div>
                         </form>
                         <form method="POST" id="form_eliminar_<?php echo $row['id']?>" action="">
                         <input type="hidden" name="eliminar" value="<?php echo $row['id']?>"/>
@@ -93,6 +131,13 @@
          var formulario = document.getElementById(id);
          formulario.submit();
     }
+
+    function mostrarTodo(e){
+       
+         var formulario = document.getElementById("formMostrarTodo");
+         formulario.submit();
+    }
+
 </script>
     
 </body>
