7c57802112a410ba11839f75d60a12f044b86964
XArcangelll <Xx-Arcangel@hotmail.com>
2023-01-25 13:17:57 -0500

    autenticacion

---
+++ autenticacion/colab.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["rol"])){
+    header("location: login.php");
+}else{
+    if($_SESSION["rol"]!=2){
+        header("location: login.php");
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colaborador</title>
+</head>
+<body>
+    <h1>Colaborador</h1>
+</body>
+</html>
